48be78b981eef5b9b623610770c66b191f31b449
Lancewer <lancewer@icloud.com>
2015-09-15 10:26:15 +0800

    Initial Commit

---
+++ CIDemo01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CIDemo01
+//
+//  Created by Lawrence on 9/15/15.
+//  Copyright (c) 2015 Lawrence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
